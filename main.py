eb1cfb398fdfd5203bcdb820abd4c0c35ed73cbd
Donovan <triops124@gmail.com>
2020-01-23 20:56:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from machine import Pin
 relay = Pin(12, Pin.OUT)
 import ConnectWiFi
 ConnectWiFi.connect()
-print ('booting up')
 while True:
   checkbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
   time.sleep(13)
