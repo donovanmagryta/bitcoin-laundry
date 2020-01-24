7328c1e74b5287a15dec1cbd1aaf5a627fdbd9d7
Donovan <triops124@gmail.com>
2020-01-23 22:19:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ relay = Pin(12, Pin.OUT)
 import ConnectWiFi
 ConnectWiFi.connect()
 while True:
-  checkbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
+  checkbal = urequests.get('http://blockchain.info/api/q/addressbalance/14wRAXh2Qrm84vUwuJ4N9xzUw6WXtqC6eR?confirmations=6')
   checkbal = int(checkbal)
   time.sleep(13)
-  recheckbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
+  recheckbal = urequests.get('http://blockchain.info/api/q/addressbalance/14wRAXh2Qrm84vUwuJ4N9xzUw6WXtqC6eR?confirmations=6')
   recheckbal = int(recheckbal)
   if (checkbal < recheckbal):
       if (recheckbal - checkbal > 0.001):
         relay.value(1)
-        time.sleep(13)
+        time.sleep(600)
   else:
      relay.value(0)
