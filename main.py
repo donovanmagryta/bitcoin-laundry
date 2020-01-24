77a24b4bebad28b8ece335396f34347f5d96579e
Donovan <triops124@gmail.com>
2020-01-23 20:50:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import ConnectWiFi
 ConnectWiFi.connect()
 print ('booting up')
 while True:
-  checkbal = urequests.get('')
-  time.sleep(15)
-  recheckbal = urequests.get('')
+  checkbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
+  time.sleep(13)
+  recheckbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
   if checkbal < recheckbal:
       if recheckbal - checkbal > 0.001:
         relay.value(1)
-        time.sleep(15)
+        time.sleep(13)
   else:
      relay.value(0)
   
