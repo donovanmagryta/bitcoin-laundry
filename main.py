bb1a57d4e662d12862d82c880d601da99c4b2c44
Donovan <triops124@gmail.com>
2020-01-23 20:55:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ while True:
   checkbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
   time.sleep(13)
   recheckbal = urequests.get('http://blockchain.info/api/q/addressbalance/1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj?confirmations=6')
-  if checkbal < recheckbal:
-      if recheckbal - checkbal > 0.001:
+  if (checkbal < recheckbal):
+      if (recheckbal - checkbal > 0.001):
         relay.value(1)
         time.sleep(13)
   else:
