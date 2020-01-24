b026f03f4a5c4412e627eeb86780266c1ba5cbbc
Donovan <triops124@gmail.com>
2020-01-23 20:39:19 -0500

    Add files via upload

---
+++ connectWiFi.py
@@ -0,0 +1,20 @@
+def connect():
+    import network
+
+    ssid = "mywifi"
+    password =  "mywifipassword"
+
+    station = network.WLAN(network.STA_IF)
+ 
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+
+    station.active(True)
+    station.connect(ssid, password)
+
+    while station.isconnected() == False:
+        pass
+
+    print("Connection successful")
+    print(station.ifconfig())
