33938c4a44046ff48d0ec9e8f19905a43f1be04e
Donovan <triops124@gmail.com>
2020-01-23 20:38:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import urequests
 import network
 import time
 import machine
-import neopixel
+from machine import Pin
+relay = Pin(12, Pin.OUT)
+
 import ConnectWiFi
 ConnectWiFi.connect()
 print ('booting up')
 while True:
   checkbal = urequests.get('https://blockchain.info/balance?active=$address')
-  time.sleep(5)
+  time.sleep(15)
   recheckbal = urequests.get('https://blockchain.info/balance?active=$address')
   if checkbal < recheckbal:
+      if recheckbal - checkbal > 0.001:
+        relay.value(1)
+        time.sleep(15)
+  else:
+     relay.value(0)
   
   
       
