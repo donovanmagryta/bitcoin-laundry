a5ffe29e328d5f5f80f1a85734c6e9b94ec47568
Donovan <triops124@gmail.com>
2020-01-28 11:10:32 -0500

    Create paypal.py

---
+++ paypal.py
@@ -0,0 +1,22 @@
+deviceid = ""
+url = "http://tipscale.digital/paypalipn.php?iot=" + deviceid
+import urequests
+import network
+import time
+import machine
+from machine import Pin
+relay = Pin(12, Pin.OUT)
+import ConnectWiFi
+ConnectWiFi.connect()
+while True:
+  checkbal = urequests.get(url)
+  checkbal = float(checkbal)
+  time.sleep(13)
+  recheckbal = urequests.get(url)
+  recheckbal = float(recheckbal)
+  if (recheckbal > checkbal):
+      if (recheckbal - checkbal > 0.0002):
+          relay.value(1)
+          time.sleep(600)
+  else:
+     relay.value(0)
