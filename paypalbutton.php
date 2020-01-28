86317a709454d8cd3efbe4ee724290d6d6728eae
Donovan <triops124@gmail.com>
2020-01-28 10:59:31 -0500

    Create paypalbutton.php

---
+++ paypalbutton.php
@@ -0,0 +1,21 @@
+ echo "<h3>Active machine #</h3>";
+ echo $iot;
+echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+  <input type="hidden" name="business" value="torchiveinc@gmail.com">
+  <input type="hidden" name="no_shipping" value="0">
+  <input type="hidden" name="cmd" value="_xclick">
+  <input type="hidden" name="amount" value="0.20">
+  <input type="hidden" name="currency_code" value="USD">
+  <input type="hidden" name="return" value="http://tipscale.digital/paypalbutton.php?iot='.$iot.'">
+  <input type="hidden" name="custom" value="'.$iot.'">
+  <input type="hidden" name="cm" value="'.$iot.'">
+  <input type="hidden" name="item_name" value="device ID # '.$iot.'">
+  <input type="hidden" name="item_number" value="'.$iot.'">
+  <input name="notify_url" value="http://tipscale.digital/paypalipn.php" type="hidden">
+  <input type="image" name="submit" border="0"
+  src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
+  alt="Buy Now">
+  <img alt="" border="0" width="1" height="1"
+  src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">
+</form>';
+?>
