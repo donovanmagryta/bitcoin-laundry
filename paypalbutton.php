3e3f49862c1af5e3ce6372f002aa03c3604aded5
Donovan <triops124@gmail.com>
2020-01-28 11:06:20 -0500

    Update paypalbutton.php

--- paypalbutton.php
+++ paypalbutton.php
@@ -1,12 +1,19 @@
- echo "<h3>Active machine #</h3>";
- echo $iot;
+ <?php
+$iot = urldecode($_GET["iot"]);
+$return = urldecode($_GET["returned"]);
+if ($return == "returned") {
+ echo "Payment Received. Device will activate within 15 seconds.";
+}
+else {
+echo "<h3>Pay to activate machine #</h3>";
+echo $iot;
 echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
   <input type="hidden" name="business" value="torchiveinc@gmail.com">
   <input type="hidden" name="no_shipping" value="0">
   <input type="hidden" name="cmd" value="_xclick">
   <input type="hidden" name="amount" value="0.20">
   <input type="hidden" name="currency_code" value="USD">
-  <input type="hidden" name="return" value="http://tipscale.digital/paypalbutton.php?iot='.$iot.'">
+  <input type="hidden" name="return" value="http://tipscale.digital/paypalbutton.php?returned=returned">
   <input type="hidden" name="custom" value="'.$iot.'">
   <input type="hidden" name="cm" value="'.$iot.'">
   <input type="hidden" name="item_name" value="device ID # '.$iot.'">
@@ -18,4 +25,5 @@ echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
   <img alt="" border="0" width="1" height="1"
   src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">
 </form>';
+}
 ?>
