00351b7c26f2579998c6e970eca3880b0b84a312
Donovan <triops124@gmail.com>
2020-01-29 11:31:38 -0500

    Update paypal.py

--- paypal.py
+++ paypal.py
@@ -1,5 +1,11 @@
-deviceid = ""
-url = "http://tipscale.digital/paypalipn.php?iot=" + deviceid
+
+deviceid = "1"
+delay = "13"
+ontime = "600"
+amount = "0.25"
+host = "http://example.com"
+ipnfilename = "/paypalipn.php"
+url = host + ipnfilename + "?iot=" + deviceid
 import urequests
 import network
 import time
@@ -10,13 +16,21 @@ import ConnectWiFi
 ConnectWiFi.connect()
 while True:
   checkbal = urequests.get(url)
+  print(url)
+  checkbal = (checkbal.text)
   checkbal = float(checkbal)
-  time.sleep(13)
+  print(checkbal)
+  time.sleep(delay)
   recheckbal = urequests.get(url)
+  recheckbal = (recheckbal.text)
   recheckbal = float(recheckbal)
+  print(recheckbal)
   if (recheckbal > checkbal):
-      if (recheckbal - checkbal > 0.0002):
-          relay.value(1)
-          time.sleep(600)
+    if (recheckbal - checkbal >= amount):
+      print("payment received")
+      relay.value(1)
+      time.sleep(ontime)
+      print("on for 600 seconds")
   else:
      relay.value(0)
+     print("off")
