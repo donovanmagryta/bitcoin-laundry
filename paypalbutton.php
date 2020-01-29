dbed7c10c8c19f70dc9e4be36f4f2162c0764e19
Donovan <triops124@gmail.com>
2020-01-29 11:37:02 -0500

    Update paypalbutton.php

--- paypalbutton.php
+++ paypalbutton.php
@@ -1,4 +1,7 @@
 <?php
+$amount = "0.25";
+$url = "example.com";
+$paypalemail = "me@example.com";
 $iot = urldecode($_GET["iot"]);
 $return = urldecode($_GET["returned"]);
 if ($return == "returned") {
@@ -8,17 +11,17 @@ else {
 echo "<h3>Pay to activate machine #</h3>";
 echo $iot;
 echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-  <input type="hidden" name="business" value="torchiveinc@gmail.com">
+  <input type="hidden" name="business" value="'.$paypalemail.'">
   <input type="hidden" name="no_shipping" value="0">
   <input type="hidden" name="cmd" value="_xclick">
-  <input type="hidden" name="amount" value="0.25">
+  <input type="hidden" name="amount" value="'.$amount.'">
   <input type="hidden" name="currency_code" value="USD">
-  <input type="hidden" name="return" value="http://tipscale.digital/paypalbutton.php?returned=returned">
+  <input type="hidden" name="return" value="http://"'.$url.'"/paypalbutton.php?returned=returned">
   <input type="hidden" name="custom" value="'.$iot.'">
   <input type="hidden" name="cm" value="'.$iot.'">
   <input type="hidden" name="item_name" value="device ID # '.$iot.'">
   <input type="hidden" name="item_number" value="'.$iot.'">
-  <input name="notify_url" value="http://tipscale.digital/paypalipn.php" type="hidden">
+  <input name="notify_url" value="http://"'.$url.'"/paypalipn.php" type="hidden">
   <input type="image" name="submit" border="0"
   src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif"
   alt="Buy Now">
